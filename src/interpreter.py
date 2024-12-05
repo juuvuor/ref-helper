6184fea62e02f64d46db2313e8062568fecd787a
Joni Rapo <jonirapo@gmail.com>
2024-12-05 13:08:14 +0200

    suunnittelua ja lainausmerkkituki argseille

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,3 +1,5 @@
+import re
+
 
 class Interpreter:
     """ Komentotulkki. """
@@ -9,20 +11,26 @@ class Interpreter:
     def executeline(self):
         """ Suorittaa yhden rivin. """
         line = self.io.read("ref-helper> ")
-        parts = line.split(" ")
-        command = self.get_command(parts)
+        args = self.to_args(line)
+        command = self.get_command(args)
         if command == None:
             # TODO: Muuta viesti paremmaks ja mahollisesti jopa ulkoista se.
             self.io.write("Tunnistamaton komento. help auttaa")
-            return
-        # NOTE: Dict => class?
-        command["execute"](self.io, self.data_manager, parts)
+            return 0
+        command["execute"](self.io, self.data_manager, args)
         return 0 # NOTE: Virheen tapahtuessa muuta kuin nolla
+        # Käytännössähän tämä on turha, koska virhe catchataan metodin ulkopuolella ja sitä kautta saadaan detailed error message printattua
+    
+    def to_args(self, str: str):
+        """
+        Jakaa merkkijonon osiin huomioiden lainausmerkit.
+        src: https://stackoverflow.com/questions/554013/regular-expression-to-split-on-spaces-unless-in-quotes
+        """
+        return re.findall('\\w+|"[\\w\\s]*"', str)
 
-    def get_command(self, parts):
+    def get_command(self, args):
         """ Hakee ensimmäistä merkkijonoa vastaavan komennon. """
         for command in self.commands:
-            # NOTE: Dict => class?
             for alias in command["alias"]:
-                if alias == parts[0]:
+                if alias == args[0]:
                     return command
