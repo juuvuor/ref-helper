46943dc22536de2fa709ecf956b08a3976759fe6
Niko Iljin <iljinisa@student.jyu.fi>
2024-12-05 08:58:59 +0200

    ohjelman ajaminen muutettu apin kautta, jotta voidaan ajaa testeistä

--- src/index.py
+++ src/index.py
@@ -3,15 +3,16 @@ from console_io import ConsoleIO
 from bibtex_manager import BibtexManager
 from commands.index import commands
 import signal
+from app import App
 
 # CTRL+C ei kaada ohjelmaa
 signal.signal(signal.SIGINT, lambda sig, frame : exit(0))
 
 # TODO: Keksi parempi tiedostonimi
-bibtex_file_path = "./test.bib"
+bibtex_file_path = "../test.bib"
 data_manager = BibtexManager(bibtex_file_path)
 console_io = ConsoleIO()
 
 i = Interpreter(console_io, data_manager, commands)
-while True:
-    i.executeline()
+app = App()
+app.run(i)
