bb867c4e39d15c42d818448a8623c39f5a095f87
noobz <remi.l.lehtinen@student.jyu.fi>
2024-12-09 15:03:15 +0200

    uusi komento edit. pystyy editoimaan vanhoja referenssejä. tekee uuden referenssin jos editoi referenssiä jota ei vielä olemassa

---
+++ src/commands/edit_reference.py
@@ -0,0 +1,27 @@
+import sys, pdb # debug
+
+alias = ["edit_reference", "edit_ref", "edit", "e"]
+def execute(io, data_manager, args):
+    #pdb.Pdb(stdout=sys.__stdout__).set_trace() # debug
+    try:
+        key_to_edit = args[1]
+        # tässä poikkeus jos avainta ei löydy
+        (entry_type, fields) = prompt_for_reference(io)
+        data_manager.update_reference(key_to_edit, entry_type, fields)
+        io.write(f"Päivitetty lähde {key_to_edit}, {entry_type}, {fields}.")
+    except Exception as e:
+        io.write(f'Lähdeavaita "{key_to_edit}" ei löydetty!')    
+
+def prompt_for_reference(io):
+    """
+    Kysyy käyttäjältä lähdeviitten uudet tiedot ja päivittää ne.
+    """
+    entry_type = io.read("Anna lähdeviitten tyyppi: ")
+    fields = {}
+    while True:
+        field_name = io.read("Anna kentän nimi: ")
+        if not field_name:
+            break
+        field_value = io.read("Anna kentän arvo: ")
+        fields[field_name] = field_value
+    return (entry_type, fields)
--- src/commands/index.py
+++ src/commands/index.py
@@ -1,9 +1,11 @@
 import commands.list as c_list
 import commands.add_reference as c_add_reference
+import commands.edit_reference as c_edit_reference
 
 commands = [
     { "alias": c_list.alias, "execute": c_list.execute },
     { "alias": c_add_reference.alias, "execute": c_add_reference.execute },
+    { "alias": c_edit_reference.alias, "execute": c_edit_reference.execute },
     {
         "alias": ["help", "h"],
         "execute": lambda io, data_manager, args : io.write("Usage: TODO") # TODO
