bc6ec8ed36cd6c2aa5e0c9b62361c7d24104770b
Joni Rapo <jonirapo@gmail.com>
2024-12-13 14:43:22 +0200

    Määritelty argumentit add-komennolle

--- src/commands/add_reference.py
+++ src/commands/add_reference.py
@@ -9,17 +9,22 @@ aliases = ["add", "a"]
 
 def add_to_subparsers(parser, subparsers):
     """ Pyytää subparseria lisäämään komennon """
-    subparsers.add_parser(
+    parser_add = subparsers.add_parser(
         "add",
         aliases=aliases,
         add_help=False,
         help="add a reference"
     )
     # TODO: Lisää argumentti --doi
+    parser_add.add_argument("--doi", nargs=1, help="Uses dx.doi.org to resolve the URL")
+    parser_add.add_argument("--url", nargs=1, help="HTTP(S) requests the URL with header {accept: application/x-bibtex}")
+
 
 def execute(io: ConsoleIO, data_manager: BibtexManager, ns: argparse.Namespace):
-    """ Suorittaa koomennon """
-    # TODO: jos ns.doi määritelty, niin hae se ja callaa data_manager.add_reference
+    """ Suorittaa komennon """
+    if ns.doi or ns.url:
+        return resolve_reference_from_arguments(io, ns)
+
     (key, entry_type, fields) = prompt_for_reference(io)
     #pdb.Pdb(stdout=sys.__stdout__).set_trace() # debug
     try:
@@ -36,7 +41,8 @@ def execute(io: ConsoleIO, data_manager: BibtexManager, ns: argparse.Namespace):
     except Exception as e:
         io.write(f'Lähdeavain "{key}" on jo olemassa!')
 
-def prompt_for_reference(io):
+
+def prompt_for_reference(io: ConsoleIO):
     """
     Kysyy käyttäjältä lähdeviitten tiedot ja lisää ne.
     """
@@ -52,6 +58,18 @@ def prompt_for_reference(io):
     return (key, entry_type, fields)
 
 
+def resolve_reference_from_arguments(io: ConsoleIO, ns: argparse.Namespace):
+    print("DEBUG: " + str(ns))
+    if ns.doi and ns.url:
+        return io.write("Defined both --doi and --url, define only one.")
+    if ns.doi:
+        pass
+    elif ns.url:
+        pass
+    else:
+        raise Exception("Tried to resolve from arguments without doi or url.")
+
+
 def get_doi(doi: str, mime_type = "application/x-bibtex"):
     """
     Hakee DOI:ta vastaavan dokumentin halutussa formaatissa.
@@ -63,3 +81,4 @@ def get_doi(doi: str, mime_type = "application/x-bibtex"):
     headers = {"accept": mime_type}
     r = requests.get(url, headers=headers, timeout=5000)
     return (r.headers.get("content-type"), r.text)
+
